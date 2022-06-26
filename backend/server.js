20109c076171dfb797463d2bdf6e687f26a0aed8
oakland-code <info@oaklandcode.org>
2022-06-26 14:02:33 -0700

    added express async handler

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
 const morgan = require('morgan');
-
+const {errorHandler} = require('./middleware/errorMiddleware')
 const app = express();
 
+// Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
 app.use(morgan('tiny'))
 
 app.use('/api/goals', require('./routes/goalRoutes') )
 
-
-
+// This function overrides the default express error handler
+ app.use(errorHandler)
 
 app.listen(port, () => {
     console.log(`Server running on PORT: ${port}...`);
