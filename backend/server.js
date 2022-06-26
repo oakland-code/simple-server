162d9da9dec4d88c27f2616f311329b7c108fd6c
oakland-code <info@oaklandcode.org>
2022-06-26 04:12:25 -0700

    http dummy routes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5000;
-
+const morgan = require('morgan');
 
 const app = express();
+
+app.use(morgan('tiny'))
+
+app.use('/api/goals', require('./routes/goalRoutes') )
+
+
 
 
 app.listen(port, () => {
